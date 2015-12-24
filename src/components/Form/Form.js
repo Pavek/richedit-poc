683e4b7ca75a3796cb4c079d0c36678a41cd521e
Pavel Zhytko <pavel.zhytko@specific-group.by>
2015-12-24 18:18:33 +0300

    add lib selector & TinyMCE lib

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import { Input, ButtonInput } from 'react-bootstrap';
+import { Input, ButtonInput, FormControls } from 'react-bootstrap';
 import ReactQuill from 'react-quill';
+import TinyMCE from 'react-tinymce'; // requires global tinymce var!
 
 import './quill.base.css';
 import './quill.snow.css';
@@ -11,12 +12,15 @@ class Form extends Component {
         this.submit = this.submit.bind(this);
         this.onEmailChange = this.onEmailChange.bind(this);
         this.onTextChange = this.onTextChange.bind(this);
+        this.libs = ['Quill', 'TinyMCE'];
+
         this.state = {
             form: {
                 to: localStorage['email'] || '',
                 body: ''
             },
-            loading: false
+            loading: false,
+            selectedLib: this.libs[0]
         };
     }
 
@@ -42,31 +46,72 @@ class Form extends Component {
         localStorage['email'] = value;
     }
 
-    onTextChange(value) {
+    onTextChange(fn) {
+        return (arg) => {
+            this.setState({
+                form: {
+                    ...this.state.form,
+                    body: fn(arg)
+                }
+            });
+        };
+    }
+
+    onSwitchLib(name) {
         this.setState({
-            form: {
-                ...this.state.form,
-                body: value
-            }
-        });
+            selectedLib: name
+        })
+    }
+
+    renderRichEdit() {
+        if ('Quill' === this.state.selectedLib) {
+            return (
+                <ReactQuill theme="snow"
+                            className="editor form-control"
+                            value={ this.state.form.body }
+                            onChange={ this.onTextChange(t => t) }/>
+            );
+        } else if ('TinyMCE' === this.state.selectedLib) {
+            return (
+                <TinyMCE
+                    content={ this.state.form.body }
+                    config={{
+                      plugins: 'autolink link image lists print preview',
+                      toolbar: 'undo redo | bold italic | alignleft aligncenter alignright'
+                    }}
+                    onChange={ this.onTextChange((e) => e.target.getContent()) }
+                />
+            );
+        }
     }
 
     render() {
         let form = this.state.form;
         let disabled = !(form.to && form.body) || this.state.loading;
         let btnText = !this.state.loading ? 'Send' : 'Sending';
+        let panelList = this.libs.map((name) => {
+            let selectedClass = (this.state.selectedLib === name) ? 'selected' : '';
+            return (
+                <span className={'li ' + selectedClass} onClick={ this.onSwitchLib.bind(this, name) }>{ name }</span>
+            );
+        });
 
         return (
             <form onSubmit={ this.submit }>
                 <Input ref="email" type="email" label="To:" placeholder="Your email"
                        value={ form.to }
                        onChange={ this.onEmailChange }/>
-                <ReactQuill theme="snow"
-                            className="editor form-control"
-                            value={ form.body }
-                            onChange={ this.onTextChange }/>
+
+                <div className="form-horizontal select-lib">
+                    <FormControls.Static label="Select lib:" labelClassName="col-sm-3" wrapperClassName="col-sm-9">
+                        { panelList }
+                    </FormControls.Static>
+                </div>
+
+                { this.renderRichEdit() }
+
                 <ButtonInput type="submit" value={btnText}
-                             disabled={ disabled }/>
+                             disabled={ disabled } />
             </form>
         );
     }
